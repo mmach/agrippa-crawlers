c232940434afa819489c13bc8284afee01ad05d3
mmach900 <m.mach.90@gmail.com>
2020-04-10 19:04:51 +0200

    no message

---
+++ HAGELAND_NO/index.js
@@ -0,0 +1,102 @@
+
+
+let HAGELAND_NO = (obj, arrayProduct, arrayProductGroups, channel, resolve, reject) => {
+    global.c.queue({
+        uri: obj.href,
+        forceUTF8: false,
+        headers: {
+            "Content-Type": "application/json",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": 1,
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"
+        },
+        callback: function (error, res, done) {
+            if (error) {
+                console.log("#############ERROR############")
+                console.log(error);
+                setTimeout(() => {
+                    channel.nack(msg);
+
+                    resolve();
+                    done();
+                }, 60000)
+            } else {
+                var $ = res.$;
+                //   console.log(res.body)
+                let productsList = $('.product-category.product')
+                let nextPage = $('.next.page-numbers')
+                let numberOfProduts = $('.product.type-product')
+                Object.keys(numberOfProduts).filter(item => {
+                    return isNaN(item) == false
+                }).map(item => {
+                   
+                    let product = numberOfProduts[item];
+                    let price = null;
+                    let currency = null
+
+                    try {
+                        price = product.children[1].children[2].children[0].children[0].children[0].children[1].data.trim()
+                        currency = product.children[1].children[2].children[0].children[0].children[0].children[0].children[0].data;
+
+                    } catch (err) {
+
+                    }
+                    let prod = {
+                        source: 'HAGELAND.NO',
+                        external_id: product.attribs.class.split(' ').filter(id => { return id.startsWith('post') })[0].split('-')[1],
+                        group: obj.title,
+                        link: product.children[1].children[1].children[0].attribs.href,
+                        product: product.children[1].children[1].children[0].children[0].data,
+                        price: price,
+                        currency: currency
+
+                    }
+                    arrayProduct.push(prod)
+                })
+                //console.log(obj.href)
+                //console.log(nextPage);
+                Object.keys(nextPage).filter(item => {
+                    return isNaN(item) == false
+                }).map(item => {
+                    let a = nextPage[item];
+                    arrayProductGroups.push({
+                        source: 'HAGELAND.NO',
+                        href: a.attribs.href,
+                        group_img: obj.group_img,
+                        parent: obj.title,
+                        title:  obj.title
+
+                    }
+                    )
+                });
+
+                Object.keys(productsList).filter(item => {
+                    return isNaN(item) == false
+                }).map(item => {
+                    let a = productsList[item].children;
+                    arrayProductGroups.push({
+                        source: 'HAGELAND.NO',
+                        title: a[1].children[3].prev.children[0].children[0].data.replace('\n', ''),
+                        href: a[1].attribs.href,
+                        group_img: a[1].children[0].attribs["data-src"],
+                        parent: obj.title
+                    }
+                    )
+                });
+
+                resolve();
+                done()
+
+            }
+
+
+
+
+        }
+    })
+
+
+}
+module.exports = HAGELAND_NO 
---
+++ index.js
@@ -0,0 +1,143 @@
+var amqp = require('amqplib/callback_api');
+var Crawler = require("crawler");
+const redis = require('async-redis');
+var Promise = require('bluebird');
+var HAGELAND_NO = require('./HAGELAND_NO/index.js');
+const CONN_URL = process.env.AMQP ? process.env.AMQP : 'amqp://oqxnmzzs:hUxy1BVED5mg9xWl8lvoxw3VAmKBOn7O@squid.rmq.cloudamqp.com/oqxnmzzs';
+const PREFETCH = process.env.PREFETCH ? process.env.PREFETCH : 5;
+const MAX_CONNECTIONS = process.env.MAX_CONNECTIONS ? process.env.MAX_CONNECTIONS : 10;
+
+global.c = new Crawler({
+    maxConnections: MAX_CONNECTIONS,
+    retries: 30,
+    retryTimeout: 60000,
+
+});
+
+
+
+// Queue just one URL, with default callback
+
+
+// Queue some HTML code directly without grabbing (mostly for tests)
+amqp.connect(CONN_URL, async function (error0, connection) {
+    if (error0) {
+        console.log(error0)
+        throw error0;
+    }
+
+    connection.createChannel(function (error1, channel) {
+
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'products-queue';
+
+        channel.prefetch(PREFETCH);
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, async function (msg) {
+            let arrayProductsGroup = []
+            let arrayProduct = []
+            let obj = msg.content.toString();
+            obj = JSON.parse(obj);
+            try {
+                await new Promise((resolve, reject) => {
+                    if (obj.source == 'HAGELAND.NO') {
+                        HAGELAND_NO(obj, arrayProduct, arrayProductsGroup, channel, resolve, reject)
+                    }
+                })
+
+
+                await new Promise((resolve, reject) => {
+                    amqp.connect(CONN_URL, function (errGroup, connGroup) {
+                        if (errGroup) {
+                            console.log("CONNECTION ERROR");
+                            console.log(errGroup);
+                            reject();
+                            return;
+                        }
+                        connGroup.createChannel(async function (err2, channelGroup) {
+                            if (err2) {
+                                console.log("CHANEL ERROR");
+                                console.log(err2);
+                                channelGroup.close()
+                                connGroup.close();
+                                reject();
+                                return
+
+                            } chGroup = channelGroup;
+                            channelGroup.assertQueue('products-queue', {
+                                durable: true
+                            });
+
+                            let promises = arrayProductsGroup.map(item => {
+                                return channelGroup.sendToQueue('products-queue', new Buffer(JSON.stringify(item)), { persistent: true });
+                            })
+
+                            await Promise.all(promises)
+                            channelGroup.close();
+                            connGroup.close();
+                            resolve();
+
+                        });
+                    })
+                });
+
+                await new Promise((resolve, reject) => {
+                    amqp.connect(CONN_URL, function (errItem, connItem) {
+                        if (errItem) {
+                            console.log("CONNECTION ERROR");
+                            console.log(errItem);
+                            reject();
+                            return;
+                        }
+                        connItem.createChannel(async function (err2, channelItem) {
+                            if (err2) {
+                                console.log("CHANEL ERROR");
+                                channelItem.close();
+                                connItem.close();
+                                reject();
+
+                                console.log(err);
+
+                                return
+
+                            } chItem = channelItem;
+                            channelItem.assertQueue('product-item-queue', {
+                                durable: true
+                            });
+
+                            let promises = arrayProduct.map(item => {
+                                return chItem.sendToQueue('product-item-queue', new Buffer(JSON.stringify(item)), { persistent: true });
+                            })
+                            await Promise.all(promises)
+                            channelItem.close();
+                            connItem.close();
+                            resolve()
+
+                        });
+                    })
+                });
+                console.log(obj)
+                channel.ack(msg)
+            } catch (err) {
+                console.log(err);
+                setTimeout(() => {
+                    channel.nack(msg)
+                    channel.close()
+
+                }, 60000)
+
+            }
+
+            // console.log(arrayProduct);
+            // channel.nack(msg);
+            // console.log(" [x] Received %s", msg.content.toString());
+
+        }, {
+            noAck: false
+        });
+    });
+});
